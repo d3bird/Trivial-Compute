7fa01e0e7258269b2ed0cdc7a506daa48ff75be4
Scott Crawford <dogbird9@gmail.com>
2025-07-07 01:32:53 +0000

    added the ablity to upload a file

--- src/backEnd/webServer.py
+++ src/backEnd/webServer.py
@@ -5,12 +5,17 @@ from flask import url_for
 from flask import flash
 from flask import redirect
 from werkzeug.exceptions import abort
-
+from werkzeug.utils import secure_filename
+import os
 import SQL.commonSQLfunctions
 import sqlite3
 
+UPLOAD_FOLDER = './uploadedFiles'
+ALLOWED_EXTENSIONS = {'txt', 'json', 'csv'}
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'exampleSecretKey'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -102,3 +107,35 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
